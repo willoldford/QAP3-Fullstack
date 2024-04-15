506d7585ca7c631ae2a25f249ebb2a3b13131d31
willoldford <william.oldford@hotmail.com>
2024-04-14 23:07:42 -0230

    changed db password

--- services/pg.dbSimple.js
+++ services/pg.dbSimple.js
@@ -3,7 +3,7 @@ const pool = new Pool({
   user: "postgres",
   host: "localhost",
   database: "dbSimple",
-  password: "WO6959wo",
+  password: "password",
   port: 5432,
 });
 module.exports = pool;
